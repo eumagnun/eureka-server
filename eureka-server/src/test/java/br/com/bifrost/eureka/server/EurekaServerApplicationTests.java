9145131261107bf326b1bf3d2814cca83885b4a1
eumagnun <eumagnun@gmail.com>
2020-10-04 15:01:07 -0300

    first commit

---
+++ eureka-server/src/test/java/br/com/bifrost/eureka/server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bifrost.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
